76b85edb78eeac79f2bb6858e4b855968118e334
iltioy <tema.illar2@gmail.com>
2022-10-10 18:19:38 +0300

    1

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const colors = require("colors");
+
+const express = require("express");
+const app = express();
+
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const connectDB = require("./config/db");
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === "development",
+    })
+);
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
